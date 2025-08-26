d5a437cd3f37bdcb3250cec8690f761843cd8c0c
Nikita-Stetskiy1 <Nikita.Stetskiy1@ibm.com>
2025-08-26 12:01:17 +0200

    Base HCP

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # understanding-hcp-terraform
 Understanding HCP Terraform with use cases, tutorials and tests.
+
+Most of the contents of this page will be based off on https://developer.hashicorp.com/terraform/tutorials/cloud-get-started
--- base/main.tf
+++ base/main.tf
@@ -4,7 +4,7 @@ resource "random_pet" "suffix" {
 }
 
 # Use data block when org_name is set
-data "tfe_organization" "defautl_org" {
+data "tfe_organization" "default_org" {
   count = var.org_name != "" ? 1 : 0
   name  = var.org_name
 }
@@ -14,4 +14,17 @@ resource "tfe_organization" "new_org" {
   count = var.org_name == "" ? 1 : 0
   name  = "test-${random_pet.suffix[0].id}"
   email = var.org_email
+}
+
+locals {
+  org_name = var.org_name == "" ? tfe_organization.new_org[0].name : data.tfe_organization.default_org[0].name
+}
+
+resource "tfe_workspace" "test" {
+  name         = "test-workspace"
+  organization = local.org_name
+  tags         = {
+    auto_destroy = "1d"
+  }
+  auto_destroy_activity_duration = "1d"
 }
