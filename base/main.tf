9583354a4d40aba86b4da474ce0e2b7ac5ea1bc2
Nikita-Stetskiy1 <Nikita.Stetskiy1@ibm.com>
2025-08-26 14:53:46 +0200

    Fixes because of IAM

--- base/main.tf
+++ base/main.tf
@@ -1,19 +1,4 @@
-data "aws_ami" "ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
+# VPC
 resource "aws_vpc" "vpc" {
   cidr_block = "172.16.0.0/16"
 
@@ -22,44 +7,39 @@ resource "aws_vpc" "vpc" {
   }
 }
 
-resource "aws_default_route_table" "route_table" {
-  default_route_table_id = aws_vpc.vpc.default_route_table_id
-
-  route {
-    cidr_block           = aws_vpc.vpc.cidr_block
-    gateway_id           = "local"
-  }
-
-  route {
-    cidr_block           = "0.0.0.0/0"
-    gateway_id           = aws_internet_gateway.gw.id
-  }
-}
-
+# Internet Gateway
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.vpc.id
 }
 
+# Route Table (usar la tabla por defecto)
+resource "aws_default_route_table" "route_table" {
+  default_route_table_id = aws_vpc.vpc.default_route_table_id
+
+  route {
+    cidr_block = aws_vpc.vpc.cidr_block
+    gateway_id = "local"
+  }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+# Subnet
 resource "aws_subnet" "subnet" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "172.16.10.0/24"
   availability_zone       = "${var.region}a"
-  map_public_ip_on_launch = "true"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "${var.instance_name}-subnet"
   }
 }
 
-resource "aws_network_interface" "network_interface" {
-  subnet_id   = aws_subnet.subnet.id
-  private_ips = ["172.16.10.100"]
-
-  tags = {
-    Name = "${var.instance_name}-nic"
-  }
-}
-
+# Security Group
 resource "aws_security_group" "security_group" {
   name        = "${var.instance_name}-scg"
   description = "Allow SSH inbound traffic and all outbound traffic"
@@ -70,6 +50,7 @@ resource "aws_security_group" "security_group" {
   }
 }
 
+# SG Ingress Rule (SSH)
 resource "aws_vpc_security_group_ingress_rule" "security_group_ingress_rule" {
   security_group_id = aws_security_group.security_group.id
   cidr_ipv4         = "0.0.0.0/0"
@@ -78,8 +59,9 @@ resource "aws_vpc_security_group_ingress_rule" "security_group_ingress_rule" {
   to_port           = 22
 }
 
+# EC2 Instance
 resource "aws_instance" "vm" {
-  ami                    = data.aws_ami.ami.id
+  ami                    = var.ami_id
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.subnet.id
   vpc_security_group_ids = [aws_security_group.security_group.id]
--- base/variables.tf
+++ base/variables.tf
@@ -13,5 +13,11 @@ variable "instance_type" {
 variable "instance_name" {
   type        = string
   description = "EC2 instance name"
-  default     = "test-instance"
+  default     = "default-instance"
+}
+
+variable "ami_id" {
+  type        = string
+  description = "AMI ID (must be provided, avoids DescribeImages)"
+  default     = "ami-09b024e886d7bbe74"
 }
